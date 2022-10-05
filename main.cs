809b1ed191d17183eafb7a554d5f40a4fef09359
manatg <114946998+manatg@users.noreply.github.com>
2022-10-05 14:04:28 -0400

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,42 @@
+// program to add two numbers; single line
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+   int num1; // declaration
+   int num2; // declaration
+    int answer; //Declaration 
+    num1= 30; 
+    num2= 16; // initalization 
+    answer= num1+num2; 
+
+    int a; // declaration
+    int b; // declaration
+    int c; // declaration
+    int d; // declaration
+    int answer1; // declaration
+    int answer2;
+    int answer3;
+    int answer4;
+    int answer5;
+     a= 4; 
+     b= 2; // instializtion 
+     answer1= a-b;
+     c= 8; 
+     d= 6; // instialization 
+     answer2= c-d;
+     answer3= a*d;
+     answer4= c/b;
+     answer5= b%c;
+    
+    
+     
+  
+    
+    Console.WriteLine (answer1);
+    Console.WriteLine (answer2);
+    Console.WriteLine (answer3);
+    Console.WriteLine (answer4);
+    Console.WriteLine (answer5);
+  }
+}
