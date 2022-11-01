1ef0adf72486c7d7731b965ea5382d34dfd1688c
manatg <114946998+manatg@users.noreply.github.com>
2022-11-01 13:46:35 -0400

    Add files via upload

---
+++ main (14).cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+ int i=2; //initialization
+    do 
+    {
+    Console.WriteLine (i);
+      i=i+2; //increment
+    } while(i<21);   //condition
+  }
+}
