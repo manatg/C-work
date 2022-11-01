2c7832b7db175a29056908f2991f492840ba4e6f
manatg <114946998+manatg@users.noreply.github.com>
2022-11-01 14:03:30 -0400

    Add files via upload

---
+++ main (17).cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+int i=1; //initalization
+    int sum =0;
+    for (i=1; i<11; i++)
+    {
+      sum = sum+i;
+      
+    }
+    Console.WriteLine("Sum of 10 numbers is {0}", sum);
+  }
+}
