5e968f3b08cbbf9da83f6be316eeef4b84f3071c
manatg <114946998+manatg@users.noreply.github.com>
2022-11-09 14:20:26 -0500

    Add files via upload

---
+++ main (20).cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    int a=3;
+    /* const double pi= 3.14;
+    double area, radius=5.4;
+    area= pi*radius*radius;
+    Console.WriteLine ("Area of circle");`
+    Console.WriteLine (area);
+    Console.WriteLine((55>33)||(90<3));
+      Console.WriteLine((55>33)&&(90<3));
+      Console.WriteLine(!(55==55));*/    
+    Console.WriteLine(a++ + 6-4);
+        Console.WriteLine(++a * 6 /4);
+  
+    
+  }
+}
