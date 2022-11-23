9afe2f3e9bc1d90d05d261a94cf73d11070a3fe0
manatg <114946998+manatg@users.noreply.github.com>
2022-11-23 15:27:13 -0500

    Add files via upload

---
+++ main (24).cs
@@ -0,0 +1,14 @@
+//Program will print the sum of the ood numvers between 1 and 10
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    int sum = 0;
+    for (int i = 1; i < 10; i += 2)
+    {
+      sum += i;
+    }
+    
+    Console.WriteLine (sum);
+  }
+}
