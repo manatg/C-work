2f0b5c072641a68aebb5caad10d7f3dcecae67ba
manatg <114946998+manatg@users.noreply.github.com>
2022-12-19 23:13:18 -0500

    Add files via upload

---
+++ main (39).cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    int m;
+    m=Convert.ToInt32(Console.ReadLine());
+    if(m%2==0)
+    {
+      Console.WriteLine("Even Number");
+    }
+    else
+    {
+      Console.WriteLine("Odd Number");
+    }
+    
+  }
+}
